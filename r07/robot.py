bd8fcc35d62797ae1a6ce5b71dbbeaa8d3e667ef
kamil <kamil_olszewski@op.pl>
2023-07-26 21:49:44 +0200

    first motor test

---
+++ r07/robot.py
@@ -0,0 +1,43 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+import atexit
+
+
+class Robot:
+    def __init__(self, motorhat_addr=0x6f):
+        # Inicjalizacja nakładki sterownika silników znajdującym się pod podanym adresem
+        self._mh = Raspi_MotorHAT(addr=motorhat_addr)
+
+        # Zmienne lokalne dla każdego z silników
+        self.left_motor = self._mh.getMotor(1)
+        self.right_motor  = self._mh.getMotor(2)
+
+        # Upewnienie się, że silniki się zatrzymają, gdy program przestanie działać
+        atexit.register(self.stop_motors)
+
+    def convert_speed(self, speed):
+        # Wybór trybu jazdy
+        mode = Raspi_MotorHAT.RELEASE
+        if speed > 0:
+            mode = Raspi_MotorHAT.FORWARD
+        elif speed < 0:
+            mode = Raspi_MotorHAT.BACKWARD
+
+        # Przeskalowanie prędkości
+        output_speed = (abs(speed) * 255) // 100
+        return mode, int(output_speed)
+
+    def set_left(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.left_motor.setSpeed(output_speed)
+        self.left_motor.run(mode)
+
+    def set_right(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.right_motor.setSpeed(output_speed)
+        self.right_motor.run(mode)
+
+    def stop_motors(self):
+        self.left_motor.run(Raspi_MotorHAT.RELEASE)
+        self.right_motor.run(Raspi_MotorHAT.RELEASE)
+
