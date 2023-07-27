9739caf9fb896e54a9009df0d24b5d523b8632c8
kamil <kamil_olszewski@op.pl>
2023-07-27 11:57:28 +0200

    using distance to plan obstacle avoidance

--- r09/avoid_with_rainbow.py
+++ r09/avoid_with_rainbow.py
@@ -32,3 +32,52 @@ class ObstacleAvoidingBehavior:
         show_rainbow(self.robot.leds, right_range)
         # Zapalenie ustawionych diod LED
         self.robot.leds.show()
+
+    def get_speeds(self, nearest_distance):
+        if nearest_distance >= 1.0:
+            nearest_speed = self.speed
+            furthest_speed = self.speed
+            delay = 100
+        elif nearest_distance > 0.5:
+            nearest_speed = self.speed
+            furthest_speed = self.speed * 0.8
+            delay = 100
+        elif nearest_distance > 0.2:
+            nearest_speed = self.speed
+            furthest_speed = self.speed * 0.6
+            delay = 100
+        elif nearest_distance > 0.1:
+            nearest_speed = -self.speed * 0.4
+            furthest_speed = -self.speed
+            delay = 100
+        else: # Kolizja
+            nearest_speed = -self.speed
+            furthest_speed = -self.speed
+            delay = 250
+        return nearest_speed, furthest_speed, delay
+
+    def run(self):
+        while True:
+            # Odczytanie odległości podanej w metrach
+            left_distance = self.robot.left_distance_sensor.distance
+            right_distance = self.robot.right_distance_sensor.distance
+            # Wyświetlenie odczytów z czujników
+            self.display_state(left_distance, right_distance)
+            # Ustalenie prędkości silników na podstawie odległości
+            nearest_speed, furthest_speed, delay = self.get_speeds(min(left_distance, right_distance))
+            print(f"Distances: l {left_distance:.2f}, r {right_distance:.2f}. Speeds: n: {nearest_speed}, f: {furthest_speed}. Delay: {delay}")
+            # i ruch robota
+            # Ustawienie prędkości silników
+            if left_distance < right_distance:
+                self.robot.set_left(nearest_speed)
+                self.robot.set_right(furthest_speed)
+            else:
+                self.robot.set_right(nearest_speed)
+                self.robot.set_left(furthest_speed)
+            # Zatrzymanie programu na czas określony przez zmienną delay
+            sleep(delay * 0.001)
+
+
+bot = Robot()
+behavior = ObstacleAvoidingBehavior(bot)
+behavior.run()
